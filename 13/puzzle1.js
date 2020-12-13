a1e3c2efd35d7e5d4c559748231bd4b573ae2fd1
Olivia Asch <olivia.c.asch@gmail.com>
2020-12-13 12:04:53 +0000

    Add day 13 part 1

---
+++ 13/puzzle1.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const { time } = require('console');
+
+const inputFile = path.join(__dirname, 'input.txt');
+const data = fs.readFileSync(inputFile, 'utf-8');
+
+const timestamp = Number(data.split('\n')[0]);
+const buses = data
+  .split('\n')[1]
+  .split(',')
+  .filter((bus) => bus !== 'x')
+  .map(Number);
+
+const closestDepartures = buses.map((bus) => {
+  let time = 0;
+  while (time < timestamp) {
+    time += bus;
+  }
+  return { bus, time };
+});
+
+closestDepartures.sort((a, b) => a.time - b.time);
+
+const chosenBus = closestDepartures[0];
+const timeToWait = chosenBus.time - timestamp;
+
+console.log(
+  `Bus ID multiplied by time to wait is ${chosenBus.bus * timeToWait}`
+);
